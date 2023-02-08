b23b4db4427c06c071b8618279d63448851e4b91
SamsonDm <samsondm97@gmail.com>
2023-02-08 18:52:26 +0600

    Displayed list of posts in the home page.
    Realised pagination and transfered to inc.
    Added stub-view for individual post displaing.

--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from django.views.generic import DetailView, ListView
+from .models import Post, Tag, Category
+
+class Home(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    extra_context = {'title': 'Home'}
+    paginate_by = 8
 
 def index(request: HttpRequest):
     return render(request, 'blog/index.html')
 
 def get_category(request: HttpRequest, slug: str):
+    return render(request, 'blog/index.html')
+
+def get_post(request: HttpRequest, slug: str):
     return render(request, 'blog/index.html')
